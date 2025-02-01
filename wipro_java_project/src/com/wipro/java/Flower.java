52f99ef3f9d6f776a2c67a478aa70d2e5cea9671
Varnan Gupta <varnangupta113@gmail.com>
2025-02-01 15:50:08 +0530

    Day1

---
+++ wipro_java_project/src/com/wipro/java/Flower.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class Flower {
+
+	/**
+	 * 
+	 */
+	
+	String name;
+    String color;
+    double price;
+    int petals; 
+	
+    Flower(String name, String color, double price, int petals) {
+		// TODO Auto-generated constructor stub
+    	this.name = name;
+        this.color = color;
+        this.price = price;
+        this.petals = petals;
+	}
+    
+    public void displayFlowerInfo() {
+        System.out.println("Flower Name: " + name);
+        System.out.println("Color: " + color);
+        System.out.println("Price: $" + price);
+        System.out.println("Number of Petals: " + petals);
+    }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Flower rose = new Flower("Rose", "Red", 2.5, 32);
+		rose.displayFlowerInfo();
+		
+		Flower sunflower = new Flower("Sunflower", "Yellow", 4, 25);
+		sunflower.displayFlowerInfo();
+
+	}
+
+}
