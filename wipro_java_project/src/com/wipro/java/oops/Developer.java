e20b1d7ee814542bff8e3603b07f6abb20d45937
Varnan Gupta <varnangupta113@gmail.com>
2025-02-03 12:23:04 +0530

    Day-2

--- wipro_java_project/src/com/wipro/java/oops/Developer.java
+++ wipro_java_project/src/com/wipro/java/oops/Developer.java
@@ -8,5 +8,19 @@ public class Developer extends Employee{
 	public Developer() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public static void main(String args[]) {
+		Employee employee = new Developer();
+		
+    	employee.setDepartment("Developer"); // Setting Developer department
+    	employee.setId(5678);                // Setting Developer ID
+    	employee.setName("Ayush");           // Setting Developer Name
+    	employee.setSalary(20000);           // Setting Developer Salary
+    	
+    	System.out.println("Employee Department -"+employee.getDepartment());
+    	System.out.println("Employee ID -"+employee.getId());
+    	System.out.println("Employee Name -"+employee.getName());
+    	System.out.println("Employee Salary -"+employee.getSalary());
+	}
 
 }
--- wipro_java_project/src/com/wipro/java/oops/Manager.java
+++ wipro_java_project/src/com/wipro/java/oops/Manager.java
@@ -7,6 +7,21 @@ public class Manager extends Employee {
 
 	public Manager() {
 		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public static void main(String args[]) {
+		Employee employee = new Manager();
+		
+    	employee.setDepartment("Manager");// Setting Manager department
+    	employee.setId(1357);             // Setting Manager ID
+    	employee.setName("Amit");         // Setting Manager Name
+    	employee.setSalary(30000);        // Setting Manager Salary
+    	
+    	System.out.println("Employee Department -"+employee.getDepartment());
+    	System.out.println("Employee ID -"+employee.getId());
+    	System.out.println("Employee Name -"+employee.getName());
+    	System.out.println("Employee Salary -"+employee.getSalary());
 	}
 
 }
