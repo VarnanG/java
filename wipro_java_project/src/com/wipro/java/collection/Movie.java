0239a0b9e4e357389779da72ba2bbcea72720d20
Varnan Gupta <varnangupta113@gmail.com>
2025-02-06 10:54:24 +0530

    Day-5
    Comparable implemented

---
+++ wipro_java_project/src/com/wipro/java/collection/Movie.java
@@ -0,0 +1,34 @@
+package com.wipro.java.collection;
+
+public class Movie implements Comparable<Movie>{
+	
+	private String n; // Movie Name
+	private double r; // Movie Rating
+	private int y;    // Release year of movie
+	public Movie(String n, double r, int y) {
+		super();
+		this.n = n;
+		this.r = r;
+		this.y = y;
+	}
+	
+	public int compareTo(Movie m) {
+		// sort movies in ascending 
+		// order of rating
+		return this.y - m.y;
+	}
+	
+	public String getName() {
+		return n;
+	}
+	
+	public double getRating() {
+		return r;
+	}
+
+	public int getYear() {
+		return y;
+	}
+	
+	
+}
