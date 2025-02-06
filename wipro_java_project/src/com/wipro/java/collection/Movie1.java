e2a730f5d113c0665fc56fe215b3e0cca3eb0605
Varnan Gupta <varnangupta113@gmail.com>
2025-02-06 11:41:27 +0530

    Day-5
    Comparator implement

---
+++ wipro_java_project/src/com/wipro/java/collection/Movie1.java
@@ -0,0 +1,28 @@
+package com.wipro.java.collection;
+
+public class Movie1 {
+
+	private String n; // Movie Name
+	private double r; // Movie Rating
+	private int y;    // Release year of movie
+	public Movie1(String n, double r, int y) {
+		super();
+		this.n = n;
+		this.r = r;
+		this.y = y;
+	}
+	public String getN() {
+		return n;
+	}
+	
+	public double getR() {
+		return r;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	
+	
+}
